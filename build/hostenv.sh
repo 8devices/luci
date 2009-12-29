8devices/luci

commit 8a11c3a475266aa05551fa433e0d9d5aa8bf1d2e

build: adjust hostenv.sh for OS X
git-svn-id: http://svn.luci.subsignal.org/luci/trunk@5654 ab181a69-ba2e-0410-a84d-ff88ab4c47bc

build/hostenv.sh
@@ -1,5 +1,6 @@
-#!/bin/sh
+##!/bin/sh
 export LD_LIBRARY_PATH="$1/usr/lib:$LD_LIBRARY_PATH"
+[ `uname -s` == "Darwin" ] && export DYLD_LIBRARY_PATH="$1/usr/lib:$DYLD_LIBRARY_PATH"
 export PATH="$1/bin:$1/usr/bin:$PATH"
 export LUA_PATH="$1/$2/?.lua;$1/$2/?/init.lua;;"
 export LUA_CPATH="$1/$3/?.so;;"

commit 0d8255b72e6b9e0ea6801d01c1c34306b7c1f09d

build: fix typo in hostenv.sh introduced in r5654

build/hostenv.sh
@@ -1,4 +1,4 @@
-##!/bin/sh
+#!/bin/sh
 export LD_LIBRARY_PATH="$1/usr/lib:$LD_LIBRARY_PATH"
 [ `uname -s` == "Darwin" ] && export DYLD_LIBRARY_PATH="$1/usr/lib:$DYLD_LIBRARY_PATH"
 export PATH="$1/bin:$1/usr/bin:$PATH"